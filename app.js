3ea1d02c2141898c2bea9072c9584d30947262c8
Geogsoc <elis.porter80@gmail.com>
2022-01-10 17:17:17 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const cors = require("cors");
-app.use(cors());
+
 const express = require("express");
 const apiRouter = require("./routers/api.router");
+app.use(cors());
 const app = express();
 app.use(express.json());
 app.use("/api", apiRouter);
